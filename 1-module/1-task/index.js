818d0632577650b6c32756e003376ec659677a5c
Sergey O <lucky.alakai@gmail.com>
2023-05-02 22:49:56 +0300

    решена задача по вычислению факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let temp = 1;
+  for (i = 1; i < n; i++) {
+    temp += temp * i;
+  }
+  return temp;
 }

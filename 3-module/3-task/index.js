a53eb6067035f2ea862046ec27aea87918992986
Sergey O <lucky.alakai@gmail.com>
2023-05-19 23:13:05 +0300

    написана функция camelize(str)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  const arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '-') {
+      arr[i] = '';
+      arr[i+1] = arr[i+1].toUpperCase();
+    }
+  }
+
+  return arr.join('');
 }

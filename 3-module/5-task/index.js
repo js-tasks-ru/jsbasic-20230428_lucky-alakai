573543f1e1bac860da248b3eeb384f9d51f2bedc
Sergey O <lucky.alakai@gmail.com>
2023-05-20 00:25:19 +0300

    написана функция на поиск min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,33 @@
 function getMinMax(str) {
-  // ваш код...
+  const symbolsArr = str.split('');
+  const unitsArr = [];
+  const unsortedtArr = [];
+
+  let start = 0;
+  let finish = 0;
+
+  for (let i = 0; i < symbolsArr.length; i++) {
+    if (symbolsArr[i] === ' '){
+      finish = i;
+      let unit = str.slice(start, finish).trim();
+      if (Number.isFinite(+unit)) {
+        unsortedtArr.push(+unit);
+      }
+      start = i;
+    }
+  }
+
+  let lastUnit = str.slice(start).trim();
+  if (Number.isFinite(+lastUnit)) {
+    unsortedtArr.push(+lastUnit);
+  }
+
+  let sortedArr = unsortedtArr.sort( (a, b) => { return a - b; } );
+  let result = {
+    min: sortedArr[0],
+    max: sortedArr[sortedArr.length - 1],
+  }
+
+  return result;
+
 }

19fc72c00225ec7a1756868e6b2e3dee4f6f7004
Sergey O <lucky.alakai@gmail.com>
2023-05-20 14:49:58 +0300

    исправлена задача на min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,33 +1,12 @@
 function getMinMax(str) {
-  const symbolsArr = str.split('');
-  const unitsArr = [];
-  const unsortedtArr = [];
+  let resultArr = str
+    .split(' ')
+    .filter( item => Number.isFinite(+item))
+    .sort( (a, b) => a - b );
 
-  let start = 0;
-  let finish = 0;
-
-  for (let i = 0; i < symbolsArr.length; i++) {
-    if (symbolsArr[i] === ' '){
-      finish = i;
-      let unit = str.slice(start, finish).trim();
-      if (Number.isFinite(+unit)) {
-        unsortedtArr.push(+unit);
-      }
-      start = i;
-    }
+  return resultObj = {
+    min: Number(resultArr[0]),
+    max: Number(resultArr[resultArr.length - 1]),
   }
-
-  let lastUnit = str.slice(start).trim();
-  if (Number.isFinite(+lastUnit)) {
-    unsortedtArr.push(+lastUnit);
-  }
-
-  let sortedArr = unsortedtArr.sort( (a, b) => { return a - b; } );
-  let result = {
-    min: sortedArr[0],
-    max: sortedArr[sortedArr.length - 1],
-  }
-
-  return result;
 
 }

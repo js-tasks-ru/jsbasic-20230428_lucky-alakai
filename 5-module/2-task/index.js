6e5f3e78ac0f7eb2e2db09d81d8f6c653b48bf9b
Sergey O <lucky.alakai@gmail.com>
2023-05-28 13:12:34 +0300

    решена задача на скрытие текста по нажатии кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+
+  const toggleBtn = document.querySelector('.toggle-text-button');
+  const textDiv = document.querySelector('#text');
+
+  toggleBtn.addEventListener('click', () => {
+    textDiv.hidden === false ? textDiv.hidden = true : textDiv.hidden = false;
+  })
+
+
 }
+
